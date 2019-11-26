3119e1c706d47d8e933853469b29603d49b01cd7
misha_r82 <misha_r82@mail.ru>
2019-11-26 23:28:51 +0300

    создание приложения

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {HttpLoaderService} from './http-loader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent
+{
+  content : string = 'content';
+
+  constructor(private httpLoader : HttpLoaderService){}
+  btnLoadClick()
+  {
+    this.httpLoader.loadAddr("https://www.avito.ru/rossiya").subscribe((res)=>{
+      this.content = res;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpLoaderService} from './http-loader.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
-  providers: [],
+  providers: [HttpLoaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
